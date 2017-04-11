cec45fdd0c765f04f4ee5ce4d5d5062a78772276
Marcos Ojeda <subliminal@gmail.com>
2017-04-10 20:56:15 -0700

    dispatch auth actions when firebase notices an auth change
    behold!!!
    
    ![](http://dl.dropboxusercontent.com/u/406291/Screenshots/GADH.png)
    
    this is an extremely verbose commit mostly because it includes two
    sort of related but not things:
    
    1. it includes all the action creators / types for auth
    2. it includes live code that actually uses it
    3. i added in code that we're not using yet but that we will *probably*
       use at some point in the future.
    
    by far the easiest thing is to treat this more coarsely, but i think
    it's important to acknowledge a user's cycle through the system more
    delicately than "logged in" / "logged out" otherwise you see ui that
    says "you logged out" when you've never even logged in.

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,8 +2,25 @@
 
 import type {Action} from '../actions/types';
 
-type AuthState = {};
+type AuthState = {
+  state: 'mystery' | 'logged in' | 'logged out' | 'login failed',
+  user?: {}, // TODO(marcos): fill this in once you know the shape,
+  errors?: {}
+};
 
-export default (state: AuthState = {}, action: Action) => {
+const defaultState = {
+  state: 'mystery',
+};
+
+export default (state: AuthState = defaultState, action: Action) => {
+  // TODO(marcos): fill these in
+  switch(action.type) {
+    case 'AUTH_MYSTERY':
+    case 'ATTEMPT_LOGIN':
+    case 'LOGGED_IN':
+    case 'LOGIN_FAILURE':
+    case 'LOGGED_OUT':
+      return state;
+  }
   return state;
 }
