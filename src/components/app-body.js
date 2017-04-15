302a5cba4d02670895a5e2c2174175583eeef913
Marcos Ojeda <subliminal@gmail.com>
2017-04-15 16:24:27 -0700

    tidying up login code.
    this is partly because i realized it was really gnarly when i looked at
    it this afternoon while trying to add a really gross form. It was so
    complicated that even i was unhappy trying to make any change.
    
    But also i think the previous version was making lots of stuff really
    hard to follow, so i thought it would help to rearrange this so that
    you can make changes to App or AppBody more easily.
    
    This is still a bunch of code, but i think it is more clear-headed
    because i am writing it in the light of day and not at like 1am.

--- src/components/app-body.js
+++ src/components/app-body.js
@@ -1,95 +1,24 @@
 // @flow
 
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import firebase from 'firebase';
-import {handleLogin, handleLogout, authedWithProvider, loginFailed, logoutFailed} from '../actions/auth';
 
-import {AuthButtons} from './auth-buttons';
+import LoginJunk from './login-junk.js';
 
-
-const mapStateToProps = state => {
-  const loggedIn = state.auth.status === 'logged in';
-  const loggingIn = state.auth.status === 'logging in';
-  const provider = state.auth.provider;
-
-  const {user = {}} = state.auth;
-  const {displayName, photoURL} = user;
-
-  return {
-    loggedIn,
-    loggingIn,
-    provider,
-    displayName,
-    photoURL
-  }
-}
-
-type AppBodyProps = {
-  loggingIn: boolean,
-  loggedIn: boolean,
-  provider: ?string,
-  displayName: ?string,
-  photoURL: ?string,
-
-  handleLogout: () => void,
-  handleLogin: (provider: 'twitter') => void,
-  authedWithProvider: (payload: {}) => void,
-  loginFailed: (errors: {}) => void,
-  logoutFailed: (error: any) => void,
-};
 
 class SimpleAppBody extends Component {
-  props: AppBodyProps;
 
   render() {
-    const {loggingIn, loggedIn, provider} = this.props;
-    const {photoURL, displayName} = this.props;
-
     return <div>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
 
-      { loggedIn && <UserGreeting photoURL={photoURL} displayName={displayName} /> }
+      <LoginJunk />
 
-      { loggingIn
-      ? <span>Opening a popup for {provider} to log in</span>
-      : <AuthButtons loggedIn={loggedIn}
-          handleTwitterLogin={this.handleTwitterLogin}
-          handleLogout={this.handleLogout}
-        />
-      }
+
     </div>
   }
 
-  handleTwitterLogin = () => {
-    this.props.handleLogin('twitter');
-    const provider = new firebase.auth.TwitterAuthProvider();
-
-    firebase.auth().signInWithPopup(provider).then(result => {
-      this.props.authedWithProvider(result);
-    }).catch(errors => {
-      this.props.loginFailed(errors);
-    })
-  }
-
-  handleLogout = () => {
-    firebase.auth().signOut()
-      .then(this.props.handleLogout)
-      .catch(this.props.logoutFailed);
-  }
 };
 
-export default connect(
-  mapStateToProps,
-  {handleLogout, handleLogin, authedWithProvider, loginFailed, logoutFailed}
-)(SimpleAppBody);
-
-
-const UserGreeting = ({photoURL, displayName}) => (
-  <div>
-    <p>{displayName}, you logged in!</p>
-    <div><img src={photoURL} /></div>
-  </div>
-);
+export default SimpleAppBody;
