64fb6d9ffe0c5a6b99938a61bdf67a09847b872e
chia berry <analuciaby@gmail.com>
2017-04-18 16:41:15 -0500

    My First Components
    FancyInput aka the text inputs

--- src/components/app-body.js
+++ src/components/app-body.js
@@ -5,6 +5,11 @@ import React, {Component} from 'react';
 import LoginJunk from './login-junk.js';
 
 
+
+const FancyInput = (props) => {
+		return <label> {props.label} <input type="text" /></label>;
+	  }
+
 class SimpleAppBody extends Component {
 
   render() {
@@ -14,6 +19,11 @@ class SimpleAppBody extends Component {
       </p>
 
       <LoginJunk />
+
+
+      <FancyInput label="Amount (g)"/>
+      <FancyInput label="Time" />
+
 
 
     </div>
