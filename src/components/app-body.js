ab4c2a4f4b3edfb351315d03e4954702f57e2633
chia berry <analuciaby@gmail.com>
2017-04-27 15:47:10 -0500

    Putting coffee bags on your screen
    Seeing what’s on your shelf

--- src/components/app-body.js
+++ src/components/app-body.js
@@ -6,9 +6,64 @@ import LoginJunk from './login-junk.js';
 
 
 
-const FancyInput = (props) => {
-    return <label> {props.label} <input name={props.name} type="text" /></label>;
+// my test variables
+
+var arr = ["Tweed", "Counter Culture", "Yirg"];
+
+var testShelf = {
+  Shelf: [
+    {
+      coffeeName: "La Laguna", 
+      roasterName: "Kuma Coffee", 
+      roastDate: 1495324800000, 
+      coffeeSize: "12 oz",
+      gone: false
+    }, 
+    {
+      coffeeName: "Timepiece", 
+      roasterName: "Tweed Coffee", 
+      roastDate: 1494028800000, 
+      coffeeSize: "16 oz",
+      gone: true
+    }, 
+    {
+      coffeeName: "Lotus", 
+      roasterName: "Barrett's", 
+      roastDate: 1492214400000,
+      coffeeSize: "16 oz",
+      gone: true
     }
+  ]
+};
+
+
+// components
+
+const FancyInput = (props) => {
+  return <label className="input-label"> {props.label} <input name={props.name} type="text" /></label>;
+}
+
+const Item = ({data}) => {
+  return (
+    <div className="coffee-list">
+      <p>{data.coffeeName} by {data.roasterName}</p>
+      <p>Roasted on {Date.parse(data.roastDate)} </p>
+    </div>)
+  
+  //render a strikethrough if gone is true
+
+}
+
+const CoffeeShelf = () => { 
+  return (
+    <div>
+      {testShelf.Shelf.map(coffee =>  <Item data={coffee} key={coffee.roastDate} />)}
+    </div>
+  )
+
+}
+
+
 
 class SimpleAppBody extends Component {
 
@@ -24,6 +79,8 @@ class SimpleAppBody extends Component {
       <FancyInput label="Amount (g)" name="amount" />
       <FancyInput label="Time" name="time" />
 
+
+      <CoffeeShelf shelfData={testShelf} />
 
 
     </div>
