aac787c5e9b9af3ec9f1f70e5f1ddee53c09fa01
chia berry <analuciaby@gmail.com>
2017-05-01 15:36:08 -0500

    Fixing spacing

--- src/components/input-stuff.js
+++ src/components/input-stuff.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 
-
 const FancyInput = (props) => {
-  return <label className="input-label"> {props.label} <input name={props.name} type="text" /></label>;
+  return (
+  	<label className="input-label"> {props.label} 
+  	<input name={props.name} type="text" />
+  	</label> )
 }
 
+// const FancyDropdown = (props)
 
 
 export default FancyInput;
