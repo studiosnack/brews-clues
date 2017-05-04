06659394c75caeb745e9a7f759a23a2045d13433
Marcos Ojeda <subliminal@gmail.com>
2017-05-04 09:15:15 -0700

    update CoffeeShelf and Item to render using flexbox

--- src/components/test-shelf.js
+++ src/components/test-shelf.js
@@ -3,19 +3,19 @@ import React from 'react';
 
 const Item = ({data}) => {
   return (
-    <div className="coffee-list">
+    <div className="coffee-item">
       <p>{data.coffeeName} by {data.roasterName}</p>
       <p className="coffee-size">{data.coffeeSize}</p>
       <p>Roasted on {data.roastDate} </p>
     </div>)
-  
+
   //render a strikethrough if gone is true
 
 }
 
-const CoffeeShelf = ({shelfData}) => { 
+const CoffeeShelf = ({shelfData}) => {
   return (
-    <div>
+    <div className="coffee-list">
       {shelfData.Shelf.map(coffee =>  <Item data={coffee} key={coffee.roastDate} />)}
     </div>
   )
