c3f89bbe6f200d78ca695de0fd351fcb9e3aa338
Marcos Ojeda <subliminal@gmail.com>
2017-10-22 14:45:36 -0700

    change all the input stuff to work in componentDidMount
    this was possible in render, but it was totally superfluous to put it there
    when it could have been in componentDidMount

--- src/database/data.js
+++ src/database/data.js
@@ -4,7 +4,6 @@ import React from 'react';
 
 import firebase from 'firebase';
 
-
 // this is a type that firebase exposes, for our purposes
 // we only care about val() and key. there are more listed here:
 // https://firebase.google.com/docs/reference/node/firebase.database.DataSnapshot
@@ -14,7 +13,6 @@ type DataSnapshot<T> = {
   ref: string,
 };
 
-
 type Props = {
   // the path in the db we are querying.
   location: string,
@@ -23,12 +21,13 @@ type Props = {
   // the data at the location we asked for, so at some point we
   // would want to maybe filter this, but for now it's ok to just
   // get all of them.
-  onChildAdded?: (snapshot: DataSnapshot<*>) => void,
+  onChildAdded?: (snapshot: DataSnapshot<*>) => mixed,
   // Same diff with a child removed, it gives you the snapshot of
   // the data when it was removed. mostly this would be useful for
   // getting the key because then we could remove it from our redux
   // store.
-  onChildRemoved?: (snapshot: DataSnapshot<*>) => void,
+  onChildRemoved?: (snapshot: DataSnapshot<*>) => mixed,
+  onChildChanged?: (snapshot: DataSnapshot<*>) => mixed,
 };
 
 export default class DataConnection extends React.Component {
@@ -41,22 +40,16 @@ export default class DataConnection extends React.Component {
   // implemented `componentShouldUpdate`
   dbRef = null;
 
+  componentWillMount() {
+    this.startConnection();
+  }
+
   // Part of what's neat about react is that you don't need to use it
   // to render web components. Here, i'm using it as a way to abstract
   // the data-gathering for the app.
-  render(){
-    const {location, onChildRemoved, onChildAdded} = this.props;
-    this.dbRef = firebase.database().ref(location)
-
-    if (onChildAdded) {
-      this.dbRef.on('child_added', onChildAdded);
-    }
-    if (onChildRemoved) {
-      this.dbRef.on('child_removed', onChildRemoved);
-    }
-
+  render() {
     // don't render anything: because the component is only responsible
-    // for adding event handlers to the database, there's no need to
+    // for adding event handlers to the database.
     return null;
   }
 
@@ -64,10 +57,26 @@ export default class DataConnection extends React.Component {
     // If for some reason the database location is changed
     // then the component will update and we should nuke the
     // existing database connection
-    if(nextProps.location !== this.props.location) {
+    if (nextProps.location !== this.props.location) {
       this.endConnection();
+      this.startConnection();
     }
   }
+
+  startConnection = () => {
+    const { location, onChildRemoved, onChildAdded, onChildChanged } = this.props;
+    this.dbRef = firebase.database().ref(location);
+
+    if (onChildAdded) {
+      this.dbRef.on('child_added', onChildAdded);
+    }
+    if (onChildRemoved) {
+      this.dbRef.on('child_removed', onChildRemoved);
+    }
+    if (onChildChanged) {
+      this.dbRef.on('child_changed', onChildChanged);
+    }
+  };
 
   endConnection = () => {
     // if a connection to firebase exists when the component
@@ -77,7 +86,7 @@ export default class DataConnection extends React.Component {
       this.dbRef.off();
       this.dbRef = null;
     }
-  }
+  };
 
   componentWillUnmount() {
     this.endConnection();
